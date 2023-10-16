23ad3b6d9db0cf48d0b3c66728fea4d7e2867b9e
gpaps <gpaps@ics.forth.gr>
2023-10-16 15:29:29 +0300

    Removing debug-prints. Added a method for list_files_recursive, aka, Crawler method to fetch a list of files.

--- b_end/utils.py
+++ b_end/utils.py
@@ -6,16 +6,10 @@ from webdav_setup_config import client
 
 load_dotenv(".env")
 
+# Nextcloud Config
 NEXTCLOUD_URL = os.getenv("NEXTCLOUD_URL")
 USERNAME = os.getenv("USERNAME")
 PASSWORD = os.getenv("PASSWORD")
-
-# Nextcloud Config
-print("NEXTCLOUD_URL:", NEXTCLOUD_URL)
-print("USERNAME:", USERNAME)
-
-
-# Careful, don't expose sensitive passwords in logs.
 
 def get_file_from_nextcloud(path: str) -> bytes:
     try:
@@ -48,3 +42,45 @@ def upload_file_to_nextcloud(filename: str, content: bytes) -> tuple:
         return "success", f"File uploaded to {remote_path}"
     except requests.RequestException as e:
         return "error", str(e)
+
+
+import xml.etree.ElementTree as ET
+def list_files_recursive(remote_path="/"):
+    """
+    Recursively list files and directories in Nextcloud starting from remote_path.
+    """
+    all_files = []
+
+    # Use WebDAV PROPFIND request to fetch directory and file listings
+    headers = {
+        "Depth": "1"
+    }
+
+    response = requests.request("PROPFIND", f"{NEXTCLOUD_URL}/remote.php/dav/files/{USERNAME}{remote_path}",
+                                headers=headers, auth=(USERNAME, PASSWORD))
+
+    if response.status_code != 207:  # 207 Multi-Status is a standard success code for PROPFIND
+        raise Exception(f"Failed to list directory {remote_path}. Status code: {response.status_code}")
+
+    # Parse the XML response
+    tree = ET.ElementTree(ET.fromstring(response.content))
+
+    # XML namespaces used in the response
+    namespaces = {
+        'd': 'DAV:',
+        'oc': 'http://owncloud.org/ns'
+    }
+
+    # Extract the href value for each file/directory returned in the response
+    for href in tree.findall(".//d:href", namespaces=namespaces):
+        path = href.text.split(f"/remote.php/dav/files/{USERNAME}", 1)[-1]
+
+        if path != remote_path:
+            if path.endswith("/"):
+                # It's a directory
+                all_files.extend(list_files_recursive(path))
+            else:
+                # It's a file
+                all_files.append(path)
+
+    return all_files
