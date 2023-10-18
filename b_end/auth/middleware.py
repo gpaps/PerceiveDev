938440c9136a893eba2b04aba30b66050263d430
gpaps <gpaps@ics.forth.gr>
2023-10-18 15:58:29 +0300

    Moved the extraction logic for required_permission from outside the middleware to inside it, since it should be checked for every request. Removed the stub where you were always returning a VISITOR role. Now the function will extract the role from the JWT token. Your middleware assumes that every route will have a tag that matches a permission in PERMISSIONS.

--- b_end/auth/middleware.py
+++ b_end/auth/middleware.py
@@ -1,29 +1,22 @@
 from .roles import UserRole
 from .permissions import PERMISSIONS
-from fastapi import Request, HTTPException
+from fastapi import Request, HTTPException, Depends
 from decouple import config
 import jwt
 
 SECRET_KEY = config('JWT_SECRET_KEY')
 
-
 def extract_user_role_from_token(token: str) -> UserRole:
     """
     Decode the token and extract the user role.
-    For now, this is simulated to always return VISITOR.
-    Decode a JWT or other token format: remaining to be seen what will we use;
     """
-    # TODO: Implement JWT decoding and verification
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])  # WIll be adjusted on algorithm as necessary
+        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
         return UserRole(payload.get("role", UserRole.VISITOR))
     except jwt.ExpiredSignatureError:
         raise HTTPException(status_code=401, detail="Token has expired")
     except jwt.InvalidTokenError:
         raise HTTPException(status_code=401, detail="Invalid token")
-    # stub
-    # Always returning a VISITOR role
-    # return UserRole.VISITOR
 
 
 def has_permission(user_role: UserRole, permission: str) -> bool:
@@ -41,11 +34,15 @@ async def auth_middleware(request: Request, call_next):
 
     user_role = extract_user_role_from_token(token)
 
-    # In this dummy implementation required_permission is determined from the path.
-    # You might want to adjust this logic to fit your routes and permissions.
-    required_permission = request.url.path
+    # Fetch the route function associated with the current request
+    route_function = request.scope.get("endpoint")
 
-    if not has_permission(user_role, required_permission):
+    # Extract the required permission from the route's tags
+    required_permission = next(
+        (tag for tag in getattr(route_function, "tags", []) if tag in PERMISSIONS[user_role]), None
+    )
+
+    if not required_permission or not has_permission(user_role, required_permission):
         raise HTTPException(status_code=403, detail="Not authorized")
 
     response = await call_next(request)
