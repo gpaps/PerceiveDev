e27f1c0ab42f696cfd61ac7ac20977d1a264baad
gpaps <gpaps@ics.forth.gr>
2023-10-23 13:16:46 +0300

    Modify the login_for_access_token function to include the role in the token[RBAC].

--- b_end/fastapi_app.py
+++ b_end/fastapi_app.py
@@ -102,17 +102,15 @@ async def list_all_files():
 @app.post("/token", response_model=Token)
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
     user = get_user(users_db, username=form_data.username)
-
     print(user)
-
     if not user:
         raise HTTPException(status_code=400, detail="Incorrect username or password")
 
     if not verify_password(form_data.password, user.hashed_password):
         raise HTTPException(status_code=400, detail="Incorrect username or password")
-
+    # Embed the role in the token payload
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
+    access_token = create_access_token(data={"sub": user.username,  "role": user.role}, expires_delta=access_token_expires)
 
     return {"access_token": access_token, "token_type": "bearer"}
 
