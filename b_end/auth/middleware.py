674672395307001f9d5c0f23016b777e25c76cd6
gpaps <gpaps@ics.forth.gr>
2023-10-30 14:48:34 +0200

    added to unused function ""-> dict" and commented out

--- b_end/auth/middleware.py
+++ b_end/auth/middleware.py
@@ -19,21 +19,21 @@ ALGORITHM = "HS256"
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-def get_current_user(token: str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(
-        status_code=401, detail="Could not validate credentials", headers={"WWW-Authenticate": "Bearer"}
-    )
-    try:
-        decoded_payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = decoded_payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        user = get_user(users_db, username=username)
-        if user is None:
-            raise credentials_exception
-    except JWTError:
-        raise credentials_exception
-    return user
+# def get_current_user(token: str = Depends(oauth2_scheme))-> dict:
+#     credentials_exception = HTTPException(
+#         status_code=401, detail="Could not validate credentials", headers={"WWW-Authenticate": "Bearer"}
+#     )
+#     try:
+#         decoded_payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+#         username: str = decoded_payload.get("sub")
+#         if username is None:
+#             raise credentials_exception
+#         user = get_user(users_db, username=username)
+#         if user is None:
+#             raise credentials_exception
+#     except JWTError:
+#         raise credentials_exception
+#     return user
 
 
 def extract_user_role_from_token(token: str) -> UserRole:
@@ -42,7 +42,7 @@ def extract_user_role_from_token(token: str) -> UserRole:
     """
     try:
         decoded_payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        print(decoded_payload['role'])
+        print(decoded_payload['role'], decoded_payload['role']=='admin' )
         return UserRole(decoded_payload.get("role", UserRole.VISITOR))
     except jwt.ExpiredSignatureError:
         raise HTTPException(status_code=401, detail="Token has expired")
@@ -54,6 +54,8 @@ def has_permission(user_role: UserRole, permission: str) -> bool:
     """
     Check if the user has the required permission based on their role.
     """
+    if not permission:
+        return False
 
     return PERMISSIONS.get(user_role, {}).get(permission, False)
 
@@ -66,7 +68,7 @@ async def auth_middleware(request: Request, call_next):
     if request.url.path in PUBLIC_ENDPOINTS:
         return await call_next(request)
 
-    # Extract the token from the request headers. This assumes a header format of "Authorization: Bearer TOKEN"
+    # Extract the token from the request headers.
     auth_header = request.headers.get("Authorization", "")
     if not auth_header or "Bearer " not in auth_header:
         logging.warning("Token is missing or not formatted correctly")
@@ -79,7 +81,7 @@ async def auth_middleware(request: Request, call_next):
         logging.warning("Token is missing")
         raise HTTPException(status_code=401, detail="Token is missing")
 
-    # Log before extracting user role
+    # Extract user role from the token
     logging.info("About to extract user role from token")
     user_role = extract_user_role_from_token(token)
     logging.info(f"This User_Role is: {user_role} extracted from token from middleware,")
@@ -87,9 +89,8 @@ async def auth_middleware(request: Request, call_next):
     # Fetch the route function associated with the current request
     route_function = request.scope.get("endpoint")
 
-    # Log before checking permission
+    # Check permission for the user role against the route's tags
     logging.info("About to check required permission")
-    # Extract the required permission from the route's tags
     required_permission = next(
         (tag for tag in getattr(route_function, "tags", []) if tag in PERMISSIONS[user_role]), None
     )
@@ -98,7 +99,7 @@ async def auth_middleware(request: Request, call_next):
     has_perm = has_permission(user_role, required_permission)
     logging.info(f"Has permission: {has_perm}")
 
-    if required_permission is None or not has_permission(user_role, required_permission):
+    if required_permission is None or not has_perm:
         raise HTTPException(status_code=403, detail="Not authorized")
     else:
         response = await call_next(request)
