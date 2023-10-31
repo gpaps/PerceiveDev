b3cdda798422d358007eee46b96e8b2014f943b9
gpaps <gpaps@ics.forth.gr>
2023-10-31 11:08:34 +0200

    added we_portal in the permissions.pyt for all users to be True

--- b_end/auth/permissions.py
+++ b_end/auth/permissions.py
@@ -1,17 +1,18 @@
 from .roles import UserRole
-
 PERMISSIONS = {
     UserRole.VISITOR: {
         # "API": False,
+        "web_portal": True,
         "Tools": False,
         "Services": True,
         "Dataset": False,
         "Trained_Models": False,
         "Code_Repo": False,
-        "Change_User_Permissions": False
+        "Change_User_Permissions": False,
     },
     UserRole.EDUCATOR: {
         # "API": False,
+        "web_portal": True,
         "Tools": True,
         "Services": True,
         "Dataset": False,
@@ -21,6 +22,7 @@ PERMISSIONS = {
     },
     UserRole.EXPERT: {
         # "API": False,
+        "web_portal": True,
         "Tools": True,
         "Services": True,
         "Dataset": True,
@@ -30,6 +32,7 @@ PERMISSIONS = {
     },
     UserRole.PERCEIVE_EXPERT: {
         # "API": False,
+        "web_portal": True,
         "Tools": True,
         "Services": True,
         "Dataset": True,
@@ -39,6 +42,7 @@ PERMISSIONS = {
     },
     UserRole.PERCEIVE_DEVELOPER: {
         # "API": True,
+        "web_portal": True,
         "Tools": True,
         "Services": True,
         "Dataset": True,
@@ -59,3 +63,4 @@ PERMISSIONS = {
         "web_portal": True,
     },
 }
+
