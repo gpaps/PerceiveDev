b703b5549ac5b07a72f057f1a2010f5780314f1a
gpaps <gpaps@ics.forth.gr>
2023-11-22 16:41:30 +0200

    some line for testing but not working prob.

--- b_end/main2.py
+++ b_end/main2.py
@@ -1,10 +1,13 @@
 import threading
 from fastapi_app import app
 from grpc_server import serve_grpc
-
+from uvicorn import Config, Server
 # Start gRPC server in a separate thread
 threading.Thread(target=serve_grpc, daemon=True).start()
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    # uvicorn.run(app, host="0.0.0.0", port=8001)
+    config = Config(app=app, host="0.0.0.0", port=8001, limit_max_request_size=50 * 1024 * 1024)
+    server = Server(config)
+    server.run()
