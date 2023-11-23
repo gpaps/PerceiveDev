c55528c3cc8982b57287a12958efba5963ce8779
gpaps <gpaps@ics.forth.gr>
2023-11-23 16:24:47 +0200

    changed almost to older version

--- b_end/main2.py
+++ b_end/main2.py
@@ -7,7 +7,7 @@ threading.Thread(target=serve_grpc, daemon=True).start()
 
 if __name__ == "__main__":
     import uvicorn
-    # uvicorn.run(app, host="0.0.0.0", port=8001)
-    config = Config(app=app, host="0.0.0.0", port=8001, limit_max_request_size=50 * 1024 * 1024)
-    server = Server(config)
-    server.run()
+    uvicorn.run(app, host="0.0.0.0", port=8001)
+    # config = Config(app=app, host="0.0.0.0", port=8001, limit_max_request_size=50 * 1024 * 1024)
+    # server = Server(config)
+    # server.run()
