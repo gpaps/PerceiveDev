2779e5a173c40e8dde7557f7d40a904dbbb4eb5a
gpaps <gpaps@ics.forth.gr>
2023-12-13 11:55:55 +0200

    clean the code; 2x future-TODO; register a Self-Signed-Certificate

--- b_end/main2.py
+++ b_end/main2.py
@@ -3,24 +3,21 @@ from fastapi_app import app
 from grpc_server import serve_grpc
 import ssl
 
-cert_file = '/home/gpaps/PycharmProjects/PerceiveDev/b_end/cert.pem'
-keyfile = '/home/gpaps/PycharmProjects/PerceiveDev/b_end/key.pem'
-
 # Start gRPC server in a separate thread
 threading.Thread(target=serve_grpc, daemon=True).start()
+
+#TODO later on when the domain is specified
+# for Production with a Reverse Proxy for secure HTTPS-using either nginx or apache2
+#TODO for now the stand-alone Self-signed-Certificate.
 
 if __name__ == "__main__":
     import uvicorn
 
-    # ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-    # ssl_context.load_cert_chain('/home/gpaps/PycharmProjects/PerceiveDev/b_end/cert.pem',
-    #                             keyfile='/home/gpaps/PycharmProjects/PerceiveDev/b_end/key.pem')
     uvicorn.run(
         app,
         host="0.0.0.0",
         port=8001,
-        # reload=True,
-        # ssl_keyfile="/home/gpaps/PycharmProjects/PerceiveDev/b_end/cert.pem",
-        # ssl_certfile='/home/gpaps/PycharmProjects/PerceiveDev/b_end/key.pem'
-    )
+        ssl_keyfile="/home/gpaps/PycharmProjects/PerceiveDev/b_end/key.pem",
+        ssl_certfile="/home/gpaps/PycharmProjects/PerceiveDev/b_end/cert.pem"
 
+    )
