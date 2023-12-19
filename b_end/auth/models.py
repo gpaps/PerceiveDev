15cea68e2e33ce9a87dc38e154f72f707589f4da
gpaps <gpaps@ics.forth.gr>
2023-12-19 16:17:50 +0200

    remove validator to revise later.

--- b_end/auth/models.py
+++ b_end/auth/models.py
@@ -6,11 +6,11 @@ class User(BaseModel):
     username: str
     password: str
 
-    @validator('password')
-    def password_length_validator(cls, v):
-        if not (8 <= len(v) <= 64):
-            raise ValueError('Password must be between 8 and 64 characters')
-        return v
+    # @validator('password')
+    # def password_length_validator(cls, v):
+    #     if not (8 <= len(v) <= 64):
+    #         raise ValueError('Password must be between 8 and 64 characters')
+    #     return v
 
 
 class UserInDB(User):
